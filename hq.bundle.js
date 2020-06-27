f6756db23973a4db77905cd44e021377a5387846
Lim Chee Aun <cheeaun@gmail.com>
2020-06-27 15:21:19 +0800

    Upgrade dependencies

--- hq.bundle.js
+++ hq.bundle.js
@@ -1,11 +1,11 @@
 import MapboxLayer from '@deck.gl/mapbox/dist/esm/mapbox-layer';
-import { AmbientLight } from '@luma.gl/core/dist/esm/lighting/light-source';
+import { AmbientLight } from '@deck.gl/core/dist/esm/effects/lighting/ambient-light';
 import SunLight from '@deck.gl/core/dist/esm/effects/lighting/sun-light';
 import LightingEffect from '@deck.gl/core/dist/esm/effects/lighting/lighting-effect';
 import ScatterplotLayer from '@deck.gl/layers/dist/esm/scatterplot-layer/scatterplot-layer';
 import SolidPolygonLayer from '@deck.gl/layers/dist/esm/solid-polygon-layer/solid-polygon-layer';
 import SimpleMeshLayer from '@deck.gl/mesh-layers/dist/esm/simple-mesh-layer/simple-mesh-layer';
-import SphereGeometry from '@luma.gl/core/dist/esm/geometries/sphere-geometry';
+import SphereGeometry from '@luma.gl/engine/dist/esm/geometries/sphere-geometry';
 import msgpack from '@ygoe/msgpack';
 import polyline from '@mapbox/polyline';
 import KDBush from 'kdbush';
