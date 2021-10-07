5709a2094604eb482faa0dd81eb6fe0219fdc70c
Lim Chee Aun <cheeaun@gmail.com>
2021-10-07 20:49:20 +0800

    New rendering method for 3D trees
    Instead of plain cutting number of trees to 300 (sort by latitude?),
    the trees are "filtered" based on height. So on lower zoom level, only
    taller trees are rendered. Zoom in, the smaller/shorter trees will
    show up.
    
    Instead of creating new multiple layers per tile, only two layers are
    created. Rendering of trees are debounced.

--- 3d/3d.js
+++ 3d/3d.js
@@ -1,5 +1,4 @@
 import MapboxLayer from '@deck.gl/mapbox/dist/esm/mapbox-layer';
-import MVTLayer from '@deck.gl/geo-layers/dist/esm/mvt-layer/mvt-layer';
 import { AmbientLight } from '@deck.gl/core/dist/esm/effects/lighting/ambient-light';
 import { DirectionalLight } from '@deck.gl/core/dist/esm/effects/lighting/directional-light';
 import LightingEffect from '@deck.gl/core/dist/esm/effects/lighting/lighting-effect';
@@ -7,7 +6,9 @@ import SolidPolygonLayer from '@deck.gl/layers/dist/esm/solid-polygon-layer/soli
 import SimpleMeshLayer from '@deck.gl/mesh-layers/dist/esm/simple-mesh-layer/simple-mesh-layer';
 import { OBJLoader } from '@loaders.gl/obj';
 import circle from '@turf/circle';
+import debounce from 'just-debounce-it';
 
+import dotPath from '../assets/dot.png';
 import crownOBJPath from '../assets/crown.obj';
 
 const ACCESS_TOKEN =
@@ -25,21 +26,25 @@ const map = (window._map = new mapboxgl.Map({
 }));
 map.addControl(new mapboxgl.NavigationControl());
 
-const treesCache = new Map();
-const cleaningData = (d) => {
-  const { id, girth: _girth, height_est } = d.properties;
-  if (treesCache.has(id)) return treesCache.get(id);
-  const position = d.geometry.coordinates;
+const coord2Trunk = (position, _girth) => {
   const girth = parseFloat((_girth || '0.5').match(/[\d.]+[^\d.]?$/)[0], 10);
   const steps = 6 + (girth - 0.5) * 2; // girth: from 0.5 to 1.5
   const trunkRadius = (girth / Math.PI) * 2;
   const trunkPolygon = circle(position, trunkRadius / 1000, { steps }).geometry
     .coordinates;
+  return trunkPolygon;
+};
+
+const treesCache = new Map();
+const cleaningData = (d) => {
+  const { id, girth, height_est } = d.properties;
+  if (treesCache.has(id)) return treesCache.get(id);
+  const position = d.geometry.coordinates;
   const scale = height_est * 0.66;
   const newD = {
     id,
     position,
-    polygon: trunkPolygon,
+    polygon: coord2Trunk(position, girth),
     elevation: height_est * 0.75,
     translation: [0, 0, height_est * 0.6],
     scale: [scale * 0.1, scale * 0.1, scale * 0.135],
@@ -49,43 +54,27 @@ const cleaningData = (d) => {
   return newD;
 };
 
-const treesMVTLayer = new MapboxLayer({
-  id: 'trees-mvt',
-  type: MVTLayer,
-  data: `https://api.mapbox.com/v4/cheeaun.bptkspgy/{z}/{x}/{y}.vector.pbf?access_token=${ACCESS_TOKEN}`,
-  minZoom: 17,
-  maxZoom: 22,
-  renderSubLayers: (props) => {
-    // console.log(props.tile.dataInWGS84.length);
-    const data = props.tile.dataInWGS84
-      .filter((d) => !!d.properties.girth_size && !!d.properties.height_est)
-      .sort((a, b) => b.geometry.coordinates[1] - a.geometry.coordinates[1])
-      .slice(0, 300); // This is a performance hack, reducing the number of trees per tile
-    // console.log(props.data.length, data.length, props);
-
-    const cleanData = data.map(cleaningData);
-
-    return [
-      new SolidPolygonLayer({
-        id: props.id + '-trunk',
-        data: cleanData,
-        getFillColor: [219, 195, 154],
-        extruded: true,
-        getElevation: (d) => d.elevation,
-      }),
-      new SimpleMeshLayer({
-        id: props.id + '-crown',
-        data: cleanData,
-        mesh: crownOBJPath,
-        loaders: [OBJLoader],
-        getColor: [175, 216, 142],
-        getTranslation: (d) => d.translation,
-        getScale: (d) => d.scale,
-        getOrientation: (d) => d.orientation,
-      }),
-    ];
-  },
+const treesTrunkLayer = new MapboxLayer({
+  id: 'trees-trunk',
+  type: SolidPolygonLayer,
+  // data: cleanData,
+  getFillColor: [219, 195, 154],
+  extruded: true,
+  getElevation: (d) => d.elevation,
 });
+
+const treesCrownLayer = new MapboxLayer({
+  id: 'trees-crown',
+  type: SimpleMeshLayer,
+  // data: cleanData,
+  mesh: crownOBJPath,
+  loaders: [OBJLoader],
+  getColor: [175, 216, 142],
+  getTranslation: (d) => d.translation,
+  getScale: (d) => d.scale,
+  getOrientation: (d) => d.orientation,
+});
+
 const ambientLight = new AmbientLight({
   intensity: 2.25,
 });
@@ -100,10 +89,102 @@ const lightingEffect = new LightingEffect({
 });
 
 map.once('load', async () => {
-  map.addLayer(treesMVTLayer, 'building-extrusion-2');
-  map.setLayerZoomRange('trees-mvt', 17, 22.1);
+  map.addLayer(treesTrunkLayer, 'building-extrusion-2');
+  map.setLayerZoomRange('trees-trunk', 15, 22.1);
+  map.addLayer(treesCrownLayer, 'building-extrusion-2');
+  map.setLayerZoomRange('trees-crown', 15, 22.1);
 
-  treesMVTLayer.deck.setProps({
+  map.addSource('trees-source', {
+    type: 'vector',
+    url: 'mapbox://cheeaun.bptkspgy',
+  });
+
+  map.loadImage(dotPath, (e, image) => {
+    map.addImage('dot', image);
+  });
+
+  map.addLayer({
+    id: 'trees',
+    type: 'symbol',
+    source: 'trees-source',
+    'source-layer': 'trees',
+    minzoom: 15,
+    layout: {
+      'icon-image': 'dot',
+      // 'icon-padding': 1,
+      'icon-ignore-placement': true,
+      'icon-allow-overlap': true,
+    },
+    paint: {
+      'icon-opacity': 0,
+    },
+  });
+
+  const minZoom = 15;
+  const maxZoom = 19;
+  const minHeight = 24;
+  const maxHeight = 0;
+  const renderTrees = () => {
+    const zoom = map.getZoom();
+    if (zoom < 15) return;
+
+    // Zoom 19: show all (height > 0)
+    // Zoom 15: show trees with height > 24
+    const height =
+      ((zoom - minZoom) / (maxZoom - minZoom)) * (maxHeight - minHeight) +
+      minHeight;
+    const trees = map.queryRenderedFeatures({
+      filter: [
+        'all',
+        ['has', 'girth_size'],
+        ['has', 'height_est'],
+        ['>', 'height_est', height],
+      ],
+      layers: ['trees'],
+      validate: false,
+    });
+
+    // DEBUGGING
+    // console.log(trees.length, trees);
+    // const treesHeight = {};
+    // trees.forEach((d) => {
+    //   const { height_est } = d.properties;
+    //   if (!treesHeight[height_est]) {
+    //     treesHeight[height_est] = 1;
+    //   }
+    //   treesHeight[height_est]++;
+    // });
+    // console.log(treesHeight);
+
+    requestAnimationFrame(() => {
+      // Sort trees by height and reduce the amount
+      const sortedTrees = trees
+        .sort((a, b) => {
+          return b.properties.height_est - a.properties.height_est;
+        })
+        .slice(0, 500);
+
+      const cleanData = sortedTrees.map(cleaningData);
+
+      treesTrunkLayer.setProps({ data: cleanData });
+      treesCrownLayer.setProps({ data: cleanData });
+    });
+  };
+  const debouncedRenderTrees = debounce(renderTrees, 500);
+
+  map.on('moveend', debouncedRenderTrees);
+  map.once('idle', renderTrees);
+
+  const debouncedResize = debounce(() => {
+    const { innerWidth: w, innerHeight: h } = window;
+    const circlePadding = Math.ceil(Math.sqrt(w * w + h * h) / 200);
+    map.setLayoutProperty('trees', 'icon-padding', circlePadding);
+
+    requestAnimationFrame(renderTrees);
+  }, 1000);
+  map.on('resize', debouncedResize);
+
+  treesCrownLayer.deck.setProps({
     effects: [lightingEffect],
   });
 });
